3774e88f8ca807003dc8eb0704c168f679e48642
KinnuSingh <31557794+KinnuSingh@users.noreply.github.com>
2017-09-02 00:12:14 -0400

    Update adminBot.php

--- adminBot.php
+++ adminBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "admin bot token";
+$bot_token = "8915cd246e1314be0a7da9fd1f";
 
 // adds admin commands 
 $isAdmin = TRUE;
